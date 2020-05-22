b33204ca0fc6aa365ac942ea99c25e509da516ba
hsj <dzzxhushengjie@126.com>
2020-05-22 19:46:15 +0800

    add new API

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,5 @@
-from .baiduDL import BaiduImgsDownloader as BD
+from src.api import BD
+from src.api import SG
+from src.api import SO
+from src.api import Bing
+from src.api import VCG
---
+++ src/api/__init__.py
@@ -0,0 +1,5 @@
+from .baiduDL import BaiduImgsDownloader as BD
+from .sougouDL import SougouImgsDownloader as SG
+from .soDL import SoImgsDownloader as SO
+from .bingDL import BingImgsDownloader as Bing
+from .vcgDL import VCGImgsDownloader as VCG
