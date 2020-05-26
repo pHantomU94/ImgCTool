868dcd598575138556586ba23d668f2f1bd9af62
hsj <dzzxhushengjie@126.com>
2020-05-26 19:34:01 +0800

    update api name

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,5 +1,5 @@
-from .baiduDL import BaiduImgsDownloader as BD
-from .sougouDL import SougouImgsDownloader as SG
-from .soDL import SoImgsDownloader as SO
+from .baiduDL import BaiduImgsDownloader as Baidu
+from .sougouDL import SougouImgsDownloader as Sougou
+from .soDL import SoImgsDownloader as SO360
 from .bingDL import BingImgsDownloader as Bing
 from .vcgDL import VCGImgsDownloader as VCG
